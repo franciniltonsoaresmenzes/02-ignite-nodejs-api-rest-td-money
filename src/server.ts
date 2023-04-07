43562af770e5476308033b5e14ea520e8a6249d9
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-04-07 19:22:12 -0300

    feat: Configurando o Knex

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello NodeJs'
+app.get('/hello', async () => {
+  const table = await knex('sqlite_schema').select('*')
+  return table
 })
 
 app
